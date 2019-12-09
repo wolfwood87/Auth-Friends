131119354c35d4b645fe86f9cf3b4e5fa8bba4fe
Derek <legato_5@yahoo.com>
2019-12-08 19:12:42 -0800

    init project and set up app structure.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+
+import Login from './components/Login';
+import FriendsList from './components/FriendsList';
+import PrivateRoute from './components/PrivateRoute';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( 
+    <Router>
+      <div>
+        <nav>
+          <a>
+            <Link to="/login">Login</Link>
+          </a>
+          <a>
+            <Link to="/friends">Friends List</Link>
+          </a>
+        </nav>
+        <Switch>
+          <PrivateRoute exact path="friends" component={FriendsList} />
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+        </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
---
+++ friends/src/components/Login.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Login = () => {
+    const [cred, setCred] = useState({name: '', password: ''})
+        return (
+            <>
+            </>
+        )
+}
+
+export default Login
