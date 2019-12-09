ed7384acf576407833a71cc8d8cac10d42704b79
Derek <legato_5@yahoo.com>
2019-12-08 20:01:11 -0800

    friends list now rendering to screen.

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import {axiosWithAuth} from '../axiosWithAuth';
-
+import Friend from './Friend';
 class FriendsList extends React.Component {
+    state = {
+        friends: []
+    };
+
+    componentDidMount() {
+        this.getData();
+    }
+
+    getData = () => {
+        axiosWithAuth()
+            .get('/friends')
+            .then(res => {
+                console.log(res)
+                this.setState({
+                    friends: res.data
+                })
+            })
+            .catch(err=> console.log(err))
+    };
     render() {
         return(
-        <>
+        <> 
+            {this.state.friends.map((item, index) => (
+                <Friend key={index} item={item}/>
+            ))}
         </>
         )
     }
